f91609d8491568dfcf987994b9a0715121c07b7c
worthant <b_dvorkin@niuitmo.ru>
2024-10-05 04:08:18 +0300

    :hammer: chore: Init project

---
+++ mix.exs
@@ -0,0 +1,32 @@
+defmodule Euler.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :euler,
+      version: "0.1.0",
+      elixir: "~> 1.15",
+      start_permanent: Mix.env() == :prod,
+      deps: deps(),
+      elixirc_paths: elixirc_paths(Mix.env())
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger]
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
+      {:propcheck, "~> 1.4.1", only: [:test]}
+    ]
+  end
+
+  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(_), do: ["lib"]
+end
